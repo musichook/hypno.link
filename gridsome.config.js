347b6172808a0ebbb3ea412d458357b78df35bb2
Andrew Musichook <andrew@sigm.us>
2020-11-28 00:03:10 +0600

    Basically work without sass support

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,111 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+// Sass related
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        //path.resolve(__dirname, './src/assets/sass/_globals.sass'),
+        // or if you use scss
+        //path.resolve(__dirname, './src/assets/sass/_globals.scss'),
+        // you can also use a glob if you'd prefer
+        path.resolve(__dirname, './src/assets/css/**/*.sass'),
+        // or scss
+        //path.resolve(__dirname, './src/assets/sass/*.scss'),
+      ],
+    })
+}
+
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'hypno — link',
+  siteDescription: 'hypno — link',
+  siteUrl: 'hypno.link',
+  titleTemplate: `%s | h — l`,
+  icon: 'src/favicon.png',
+
+  // What is it?
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        ['gridsome-plugin-remark-shiki', {
+          theme: 'min-light'
+        }]
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+          author: {
+            typeName: 'Author',
+            create: true,
+          },
+        },
+      },
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'G-ET0V4N6EYQ',
+      },
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      },
+    },
+    {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'hypno.link',
+          feed_url: 'https://hypno.link/feed.xml',
+          site_url: 'https://hypno.link',
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.description,
+          url: 'https://hypno.link' + node.path,
+          author: node.author,
+          date: node.date,
+        }),
+        output: {
+          dir: './static',
+          name: 'feed.xml',
+        },
+      },
+    },
+  ],
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id',
+    Author: '/author/:id',
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "./src/assets/style/main.sass";'
+      }
+    }
+  }
+
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
+// Layout
 import DefaultLayout from '~/layouts/Default.vue'
+// Styles
+//import '~/assets/style/main.sass'
 
-export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
+
+export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+
+  head.htmlAttrs = { lang: 'en', class: '' }
+  head.bodyAttrs = { class: '' }
+
+
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,700;0,800;1,400&display=swap'
+  })
 }
